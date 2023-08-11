2795531f65130d788a88bb0e22aee96aea684ebf
AdPiscinam <lab@walimaloui.com>
2023-08-11 22:14:36 +0200

    Initial Commit

---
+++ OC12-CleanNetworkBox/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  OC12-CleanNetworkBox
+//
+//  Created by Walim Aloui on 11/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
