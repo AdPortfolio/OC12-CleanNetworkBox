2168a66b1b5d2bd9b7cf230b16a0dd54d3df0ee2
AdPiscinam <lab@walimaloui.com>
2023-08-14 21:17:52 +0200

    Delete useless Presenter and update saveMethod name in the 'CreateNameWorker'

---
+++ OC12-CleanNetworkBox/Scenes/CreateProfile/CreateNameWorker.swift
@@ -0,0 +1,13 @@
+//
+//  CreateProfileWorker.swift
+//  OC12-CleanNetworkBox
+//
+//  Created by Walim Aloui on 11/08/2023.
+
+import UIKit
+
+class CreateNameWorker {
+    func saveNameToDatabase(firstName: String) {
+        
+    }
+}
