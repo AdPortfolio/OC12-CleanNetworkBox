c6750bb8c9131946c7af96a689108f5d1272e2cf
AdPiscinam <lab@walimaloui.com>
2023-08-19 21:24:52 +0200

    Add 'Create Company' Scene files

---
+++ OC12-CleanNetworkBox/Scenes/CreateCompany/CreateCompanyWorker.swift
@@ -0,0 +1,13 @@
+//
+//  CreateCompanyWorker.swift
+//  OC12-CleanNetworkBox
+//
+//  Created by Walim Aloui on 12/08/2023.
+
+import UIKit
+
+class CreateCompanyWorker {
+    func saveCompanyNameToDataBase(companyName: String) {
+        
+    }
+}
