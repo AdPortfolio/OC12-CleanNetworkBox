2f1ca57f1f1d1c91b4e8f8ee3ed8baa0ff1393bb
AdPiscinam <lab@walimaloui.com>
2023-09-13 01:06:19 +0200

    Add ProContactRepository protocol and ProContactManager struct. Add CoreDataManager

---
+++ OC12-CleanNetworkBox/CoreData/CoreDataManager.swift
@@ -0,0 +1,38 @@
+//
+//  CoreDataManager.swift
+//  OC12-CleanNetworkBox
+//
+//  Created by Walim Aloui on 12/09/2023.
+//
+
+import Foundation
+import CoreData
+
+final class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer
+    let backgroundContext: NSManagedObjectContext
+    let mainContext: NSManagedObjectContext
+    
+    private init() {
+        persistentContainer = NSPersistentContainer(name: "DataModels")
+        let description = persistentContainer.persistentStoreDescriptions.first
+        description?.type = NSSQLiteStoreType
+        
+        persistentContainer.loadPersistentStores { description, error in
+            guard error == nil else {
+                fatalError("was unable to load store \(error!)")
+            }
+        }
+        
+        mainContext = persistentContainer.viewContext
+        
+        backgroundContext = NSManagedObjectContext(concurrencyType: .privateQueueConcurrencyType)
+        backgroundContext.mergePolicy = NSMergeByPropertyObjectTrumpMergePolicy
+        backgroundContext.parent = self.mainContext
+    }
+}
+
+
