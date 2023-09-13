0f2c427e33771c5a2a130a958e8f90be90678446
AdPiscinam <lab@walimaloui.com>
2023-09-13 16:49:25 +0200

    Add ProContact and Address Models

---
+++ OC12-CleanNetworkBox/Models/ProContact.swift
@@ -0,0 +1,37 @@
+//
+//  ProContact.swift
+//  OC12-CleanNetworkBox
+//
+//  Created by Walim Aloui on 13/09/2023.
+//
+
+import Foundation
+
+class ProContact {
+    var birthday: Date?
+    var companyLogo: Data?
+    var companyName: String?
+    var dateOfCreation: Date?
+    var department: String?
+    var email: String?
+    var facebook: String?
+    var firstName: String?
+    var function: String?
+    var github: String?
+    var id: String?
+    var instagram: String?
+    var internetLink: String?
+    var lastName: String?
+    var linkedIn: String?
+    var motto: String?
+    var paypal: String?
+    var phone: String?
+    var photo: Data?
+    var skype: String?
+    var twitter: String?
+    var website: String?
+    var youtube: String?
+    var companyAddress: Address?
+}
+
+
